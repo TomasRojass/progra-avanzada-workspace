940747d2b2ffa453287d0d26587a8c822049af3f
leandro <leandrojuan1998@gmail.com>
2024-05-20 02:00:42 -0300

    comentar codigo

--- Varilla/src/Varilla/Main.java
+++ Varilla/src/Varilla/Main.java
@@ -4,31 +4,31 @@ package Varilla;
 
 public class Main {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		int largoVarilla = 5;
 		int[] cortes = { 2, 4 };
 		System.out.println("Costo optimo:" + cortar(largoVarilla, cortes));
 	}
 	
-	//Punto de corte	Clave
+	
+	//Punto de corte	Par(inicioSubVarilla, finSubVarilla)
 	//Costo				Valor
 	
-	//HashMap<Integer, Integer> mapa = new HashMap<>();
-	//int costoOptimo = cortar(largoVarilla, cortes, mapa);
-	
 	public static int cortar(int largoVarilla, int[] cortes) {
-		//int mitad = largoVarilla / 2;
-		//int corteMasViable = buscarMitadMasCercana(cortes, mitad);
+		HashMap<Integer, Integer> mapa = new HashMap<>();
+		int costoOptimo = cortar(largoVarilla, cortes, mapa);
+		int mitad = largoVarilla / 2;
+		int corteMasViable = buscarMitadMasCercana(cortes, mitad);
 		
-		//int costo = Math.min(, );
+		int costo = Math.min(, );
 		
-		//cortar(corteMasViable, cortes, mitad - 1, cortes.length);
-		//cortar(largoVarilla - corteMasViable, cortes, 0, mitad + 1);
+		cortar(corteMasViable, cortes, mitad - 1, cortes.length);
+		cortar(largoVarilla - corteMasViable, cortes, 0, mitad + 1);
 		return largoVarilla;
 	}
 	
 	private static int cortar(int largoVarilla, int[] cortes, int inicio, int fin) {
 		return 0;
-	}
+	}*/
 
 }
