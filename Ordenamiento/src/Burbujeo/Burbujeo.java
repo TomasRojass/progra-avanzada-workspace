6bc0dadefe45efde20919919cd900e08a7e41f0f
Federico Loiero <federico.loiero95@gmail.com>
2024-05-21 23:37:04 -0300

    agrego ordenamien burbujeo

---
+++ Ordenamiento/src/Burbujeo/Burbujeo.java
@@ -0,0 +1,15 @@
+package Burbujeo;
+
+public class Burbujeo {
+	public void ordenar(int []vector){
+        for(int i=0; i<vector.length;i++){
+            for(int j=i+1;j<vector.length;j++){
+                if(vector[i]>vector[j]){
+                    int aux = vector[j];
+                    vector[j] = vector[i];
+                    vector[i] = aux;
+                }
+            }
+        }
+    }
+}
