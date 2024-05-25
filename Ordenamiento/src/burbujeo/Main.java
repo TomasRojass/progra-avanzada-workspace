b39862be9bb7b5d391658221665e2f144664af4a
Federico Nicolas Loiero <fede_loi95@hotmail.com>
2024-05-25 14:10:52 -0300

    cambios

--- Ordenamiento/src/burbujeo/Main.java
+++ Ordenamiento/src/burbujeo/Main.java
@@ -3,7 +3,7 @@ package burbujeo;
 public class Main {
 	 public static void main(String[] args) {
 		 Burbujeo burbujeo = new Burbujeo();
-	        int[] vector= {5,6,8,2,4,7,25,69,12,14,19,25};
+	        int[] vector= {5,6,8,55,89,35,46,5,14,19,25};
 	        burbujeo.ordenar(vector);
 	        for(int i = 0;i<vector.length;i++){
 	            System.out.print(vector[i] + " ");
