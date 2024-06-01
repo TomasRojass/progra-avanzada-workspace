5d4ea6cef96847d1baf729cbd4fe1eb6fbd31b7a
Juan Bianchi <juanbianchi@icloud.com>
2024-06-01 15:05:04 -0300

    doc: Agrego documentacion en la clase CortadorDeVarilla en el paquete Varilla de programacionDinamica

--- ProgramacionDinamica/src/Varilla/CortadorDeVarilla.java
+++ ProgramacionDinamica/src/Varilla/CortadorDeVarilla.java
@@ -9,6 +9,15 @@ private int iteraciones;
 		this.iteraciones = 0;
 	}
 	
+	
+	/**
+	 * Calcula el costo mínimo de cortar una varilla de longitud `largoVarilla` en las posiciones especificadas
+	 * en `cortesVarilla`, utilizando un enfoque top-down de programación dinámica con memoización.
+	 *
+	 * @param cortesVarilla Un array de enteros que indica las posiciones en las que se deben realizar los cortes.
+	 * @param largoVarilla La longitud total de la varilla.
+	 * @return El costo mínimo de realizar todos los cortes.
+	 */
 	public int cortarOptimoTopDown(int[] cortesVarilla, int largoVarilla) {
 		HashMap<CombinacionCorte, Integer> memo = new HashMap<>();
 		long startTime = System.currentTimeMillis();
@@ -26,6 +35,17 @@ private int iteraciones;
 		return ganancia;
 	}
 	
+	
+	/**
+	 * Metodo auxiliar recursiva que calcula el costo mínimo de realizar los cortes en la varilla
+	 * en el rango especificado por `izq` y `der`, utilizando memoización para evitar cálculos repetidos.
+	 *
+	 * @param cortesVarilla Un array de enteros que indica las posiciones de los cortes, incluyendo los extremos.
+	 * @param izq El índice izquierdo del rango actual en el que se están considerando los cortes.
+	 * @param der El índice derecho del rango actual en el que se están considerando los cortes.
+	 * @param memo Un HashMap que almacena los costos mínimos calculados para las combinaciones de cortes.
+	 * @return El costo mínimo de realizar los cortes en el rango especificado.
+	 */
 	private int cortarOptimoTopDown(int[] cortesVarilla, int izq, int der, HashMap<CombinacionCorte, Integer> memo) {
 		if(izq + 1 == der) {
 			return 0;
@@ -36,7 +56,9 @@ private int iteraciones;
 		}
 		
 		int minCosto = Integer.MAX_VALUE;
+		// Recorre todas las posiciones de cortes posibles en el rango actual
 		for(int i = izq + 1; i < der; i++) {
+			// Calcula el costo de cortar en la posición actual y los costos de los cortes recursivos
 			int costo = cortesVarilla[der] - cortesVarilla[izq] + cortarOptimoTopDown(cortesVarilla, izq, i, memo) + cortarOptimoTopDown(cortesVarilla,  i, der, memo);
 			if(costo < minCosto) {
 				minCosto = costo;
