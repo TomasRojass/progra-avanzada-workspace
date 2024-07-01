3894214cf484485106124b713338ce3a5dd60063
Federico Nicolas Loiero <fede_loi95@hotmail.com>
2024-06-30 23:33:22 -0300

    agrego tests a matedos de ordenamiento

---
+++ Ordenamiento/src/timsort/test.java
@@ -0,0 +1,50 @@
+package timsort;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void testEmptyArray() {
+        int[] array = {};
+        new TimSortInt().ordenar(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{}, array);
+    }
+
+    @Test
+    public void testSingleElement() {
+        int[] array = {1};
+        new TimSortInt().ordenar(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{1}, array);
+    }
+
+    @Test
+    public void testAlreadySorted() {
+        int[] array = {1, 2, 3, 4, 5};
+        new TimSortInt().ordenar(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    public void testReverseSorted() {
+        int[] array = {5, 4, 3, 2, 1};
+        new TimSortInt().ordenar(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    public void testRandomOrder() {
+        int[] array = {3, 1, 4, 5, 2};
+        new TimSortInt().ordenar(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    public void testDuplicates() {
+        int[] array = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+        new TimSortInt().ordenar(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9}, array);
+    }
+}
